68be391faa1337e89f831e51ea3336581d27c8f1
magic <272loki@gmail.com>
2026-04-29 03:46:46 +0800

    add prebuilt frontend assets

---
+++ new-api/web/dist/assets/chunk-XZIHB7SX-BMvLFjQL.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./index-Cwpsf0vF.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
